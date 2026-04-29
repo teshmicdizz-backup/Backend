fa8cedbc75c60fc2ccae8d71742481d68c30a4ce
Dev <dev@example.com>
2026-04-29 13:03:08 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import itemRoutes from "./routes/itemRoutes.js";
+import dns from "dns";
+
+dotenv.config();
+
+// Force Google DNS
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Item Manager API is running..." });
+});
+
+app.use("/api/items", itemRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error.message);
+    process.exit(1);
+  });
